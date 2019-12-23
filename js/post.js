adb51e055b1a53a616ace3eaa031b52c53e6e83a
CODE <dhruv.n.rajpurohit@gmail.com>
2019-12-23 16:49:51 +0530

    Update post.js show the modal and hide
    show the modal and hide the modal of sign in and sign up

--- js/post.js
+++ js/post.js
@@ -9,3 +9,15 @@ function hideSignInModal() {
   var modal = document.getElementById("signin-modal");
   modal.style.display = "none";
 }
+
+/* show sign up modal */
+function showSignUpModal() {
+  var modal = document.getElementById("signup-modal");
+  modal.style.display = "flex";
+}
+
+/* hide sign up modal */
+function hideSignUpModal() {
+  var modal = document.getElementById("signup-modal");
+  modal.style.display = "none";
+}

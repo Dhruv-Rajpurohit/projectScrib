4e1a8acaf85f4eebe50add698dd2cf31e927c183
CODE <dhruv.n.rajpurohit@gmail.com>
2019-12-23 16:48:37 +0530

    Create post.js Signin/SignUp modal
    Signin/SignUp modal

---
+++ js/post.js
@@ -0,0 +1,11 @@
+/* show sign in modal */
+function showSignInModal() {
+  var modal = document.getElementById("signin-modal");
+  modal.style.display = "flex";
+}
+
+/* hide sign in modal */
+function hideSignInModal() {
+  var modal = document.getElementById("signin-modal");
+  modal.style.display = "none";
+}

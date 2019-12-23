fa0f52f795bd97995b6c840bfb4ce05f939a0cdf
CODE <dhruv.n.rajpurohit@gmail.com>
2019-12-23 16:51:48 +0530

    Update post.js added save and edit content

--- js/post.js
+++ js/post.js
@@ -48,3 +48,27 @@ function addComment(id) {
     a.value = a.defaultValue;
   }
 }
+
+var titleConetnt = document.getElementById('blogTitleNew');
+var contentText = document.getElementById('blogBody');
+var editButton = document.getElementById('editButton');
+var saveButton = document.getElementById('saveButton');
+// edit and save the content
+function editContent() {
+  contentText.contentEditable = "true";
+  titleConetnt.contentEditable = "true";
+  contentText.style.border = "1px solid red";
+  titleConetnt.style.border = "1px solid red";
+  editButton.style.display = "none";
+  saveButton.style.display = "block";
+  contentText.focus();
+}
+
+function saveContent() {
+  contentText.contentEditable = "false";
+  titleConetnt.contentEditable = "false";
+  contentText.style.border = "none";
+  titleConetnt.style.border = "none";
+  editButton.style.display = "block";
+  saveButton.style.display = "none";
+}
